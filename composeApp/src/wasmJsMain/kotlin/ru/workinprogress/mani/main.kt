10b3186ebf938b1e9b43c0fb3f5bf5b3062dca8b
youndie <panic.xyb@gmail.com>
2024-11-19 15:46:53 +0700

    fix wasm

--- composeApp/src/wasmJsMain/kotlin/ru/workinprogress/mani/main.kt
+++ composeApp/src/wasmJsMain/kotlin/ru/workinprogress/mani/main.kt
@@ -1,12 +1,30 @@
 package ru.workinprogress.mani
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        App()
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Box(
+                modifier = Modifier.size(DpSize(420.dp, (420 / 9.toFloat() * 16).dp)),
+            ) {
+                App()
+            }
+        }
+
     }
 }
--- shared/src/commonMain/kotlin/ru/workinprogress/mani/today.kt
+++ shared/src/commonMain/kotlin/ru/workinprogress/mani/today.kt
@@ -4,7 +4,7 @@ import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.datetime.*
 
-inline fun <T> emptyImmutableList(): ImmutableList<T> = emptyList<T>().toImmutableList()
+fun <T> emptyImmutableList(): ImmutableList<T> = emptyList<T>().toImmutableList()
 
 fun today() = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).date
 val LocalDate?.orToday get() = this ?: today()
