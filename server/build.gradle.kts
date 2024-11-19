bde016241cdc126e7d1858454b36cc834a2f63cb
youndie <panic.xyb@gmail.com>
2024-11-19 22:20:10 +0700

    ci

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -5,8 +5,10 @@ plugins {
     application
 }
 
+val appName = "mani-backend"
+
 group = "ru.workinprogress.mani"
-version = "1.0.0"
+version = "0.0.1"
 
 application {
     mainClass.set("ru.workinprogress.mani.ApplicationKt")
