38885c4c8a4f064829309e8757a0b2fc570d9f6e
youndie <panic.xyb@gmail.com>
2024-11-21 03:53:31 +0700

    go

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,3 +1,5 @@
+import io.ktor.plugin.features.*
+
 plugins {
     alias(libs.plugins.kotlinJvm)
     alias(libs.plugins.ktor)
@@ -52,3 +54,12 @@ val copyFrontend = task<Copy>("copyFrontend") {
 }
 
 project.tasks.find { "processResources" == it.name }!!.dependsOn(copyFrontend)
+
+ktor {
+    docker {
+        jreVersion.set(JavaVersion.VERSION_21)
+        localImageName.set("mani-backend")
+        imageTag.set("0.0.1-preview")
+        customBaseImage.set("amazoncorretto:21-alpine3.20-jdk")
+    }
+}
