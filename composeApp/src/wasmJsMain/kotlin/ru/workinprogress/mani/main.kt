0d6d6ecf6ee3da48b1e12f788fc1b5af9b702c5b
youndie <panic.xyb@gmail.com>
2024-11-22 19:05:44 +0700

    fix

--- composeApp/src/wasmJsMain/kotlin/ru/workinprogress/mani/main.kt
+++ composeApp/src/wasmJsMain/kotlin/ru/workinprogress/mani/main.kt
@@ -1,13 +1,6 @@
 package ru.workinprogress.mani
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
 
@@ -15,16 +8,6 @@ import kotlinx.browser.document
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        Box(
-            contentAlignment = Alignment.Center,
-            modifier = Modifier.fillMaxSize()
-        ) {
-            Box(
-                modifier = Modifier.size(DpSize(420.dp, (420 / 9.toFloat() * 16).dp)),
-            ) {
-                App()
-            }
-        }
-
+        App()
     }
 }
