4592794b3a1ec1d8bb36b3254ffe7afb739c3416
youndie <panic.xyb@gmail.com>
2024-11-26 02:59:30 +0700

    MongoConfig: revert accept password file

--- server/src/main/kotlin/ru/workinprogress/mani/model/MongoConfig.kt
+++ server/src/main/kotlin/ru/workinprogress/mani/model/MongoConfig.kt
@@ -2,16 +2,13 @@ package ru.workinprogress.mani.model
 
 import io.ktor.server.config.*
 import org.bson.BsonValue
-import java.io.File
 
 data class MongoConfig(val userName: String = "", val password: String = "", val host: String = "") {
     companion object {
         fun ApplicationConfig.mongoConfig(): MongoConfig {
             return MongoConfig(
                 property("username").getString(),
-                property("password").getString().let { password ->
-                    File("/run/secrets/${password}").takeIf { it.exists() }?.readText() ?: password
-                },
+                property("password").getString(),
                 property("host").getString()
             )
         }
