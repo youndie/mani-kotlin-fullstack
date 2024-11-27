546b6254f7e4d8c1d55e09e31b83b414292c7949
youndie <panic.xyb@gmail.com>
2024-11-27 14:09:34 +0700

    WasmJs: Preloader

--- composeApp/src/wasmJsMain/kotlin/ru/workinprogress/mani/main.kt
+++ composeApp/src/wasmJsMain/kotlin/ru/workinprogress/mani/main.kt
@@ -1,13 +1,18 @@
 package ru.workinprogress.mani
 
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
 
-
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
+        LaunchedEffect("") {
+            document.getElementById("preloader")?.let { element ->
+                document.body?.removeChild(element)
+            }
+        }
         App()
     }
 }
