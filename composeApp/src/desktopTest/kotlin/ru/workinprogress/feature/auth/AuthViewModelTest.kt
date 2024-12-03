8dd1af8f760356fb44055647a5913767cf3b6504
youndie <panic.xyb@gmail.com>
2024-12-03 11:09:27 +0700

    LogoutTestCase

--- composeApp/src/desktopTest/kotlin/ru/workinprogress/feature/auth/AuthViewModelTest.kt
+++ composeApp/src/desktopTest/kotlin/ru/workinprogress/feature/auth/AuthViewModelTest.kt
@@ -9,29 +9,10 @@ import org.koin.dsl.module
 import org.koin.test.KoinTest
 import org.koin.test.get
 import ru.workinprogress.feature.auth.domain.AuthUseCase
-import ru.workinprogress.mani.data.ServerException
 import ru.workinprogress.feature.auth.ui.AuthViewModel
 import ru.workinprogress.feature.auth.ui.model.AuthUiState
+import ru.workinprogress.mani.data.ServerException
 import kotlin.test.*
-
-private val errorMessage = "fake error"
-
-class FakeAuthUseCase(private val success: () -> Boolean) : AuthUseCase() {
-    override suspend fun invoke(params: LoginParams) = if (success()) {
-        Result.Success(true)
-    } else {
-        Result.Error(ServerException(errorMessage, null))
-    }
-}
-
-private fun testModule(success: () -> Boolean) = module {
-    single<AuthUseCase> {
-        FakeAuthUseCase(success)
-    }
-
-    single<AuthViewModel> { AuthViewModel(get()) }
-}
-
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class AuthViewModelTest : KoinTest {
@@ -42,7 +23,6 @@ class AuthViewModelTest : KoinTest {
 
     @BeforeTest
     fun setUp() {
-        // Start Koin
         startKoin {
             modules(testModule { useCaseSuccess })
         }
@@ -97,4 +77,22 @@ class AuthViewModelTest : KoinTest {
         stopKoin()
         Dispatchers.resetMain()
     }
+}
+
+private val errorMessage = "fake error"
+
+class FakeAuthUseCase(private val success: () -> Boolean) : AuthUseCase() {
+    override suspend fun invoke(params: LoginParams) = if (success()) {
+        Result.Success(true)
+    } else {
+        Result.Error(ServerException(errorMessage, null))
+    }
+}
+
+private fun testModule(success: () -> Boolean) = module {
+    single<AuthUseCase> {
+        FakeAuthUseCase(success)
+    }
+
+    single<AuthViewModel> { AuthViewModel(get()) }
 }
