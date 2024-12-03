32f030767722780d1c48644b37ac4b5547b5f3d9
youndie <panic.xyb@gmail.com>
2024-12-03 22:42:44 +0700

    bump agp

--- composeApp/src/commonTest/kotlin/ru/workinprogress/feature/main/ui/SimpleMainTest.kt
+++ composeApp/src/commonTest/kotlin/ru/workinprogress/feature/main/ui/SimpleMainTest.kt
@@ -1,6 +1,9 @@
 package ru.workinprogress.feature.main.ui
 
-import androidx.compose.ui.test.*
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.runComposeUiTest
 import androidx.compose.ui.text.AnnotatedString
 import kotlinx.collections.immutable.persistentMapOf
 import kotlinx.datetime.LocalDate
@@ -15,9 +18,11 @@ class SimpleMainTest {
     fun mainLayoutTest() {
         runComposeUiTest {
             setContent {
-                MainContent(futureInformation = AnnotatedString("future info"), chart = {
-                    ChartComponent(ChartUi(days = persistentMapOf(LocalDate(2000, 1, 1) to 0.0)))
-                })
+                MainContent(
+                    futureInformation = AnnotatedString("future info"),
+                    chart = {
+                        ChartComponent(ChartUi(days = persistentMapOf(LocalDate(2000, 1, 1) to 0.0)))
+                    })
             }
             onNodeWithTag("chartBox").assertIsDisplayed()
             onNodeWithTag("futureInfo").assertIsDisplayed()
