8513eefba3ab573b2b0caf654148826dfbf945ff
Pavel Votyakov <panic.xyb@gmail.com>
2024-12-27 11:34:33 +0700

    fix

--- composeApp/src/commonMain/kotlin/ru/workinprogress/feature/transaction/module.kt
+++ composeApp/src/commonMain/kotlin/ru/workinprogress/feature/transaction/module.kt
@@ -1,5 +1,7 @@
 package ru.workinprogress.feature.transaction
 
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
@@ -10,6 +12,7 @@ import ru.workinprogress.feature.transaction.domain.*
 import ru.workinprogress.feature.transaction.ui.TransactionsViewModel
 
 val transactionsModule = module {
+    single { Dispatchers.Default }.bind<CoroutineDispatcher>()
     singleOf(::DeleteTransactionsUseCase)
     singleOf(::GetTransactionsUseCase)
     singleOf(::GetTransactionUseCase)
