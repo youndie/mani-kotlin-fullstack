be3b81dce4dc4e2f2ef455bb5f51aca04e660852
Pavel Votyakov <panic.xyb@gmail.com>
2025-10-28 01:55:30 +0100

    fixes

--- shared/src/commonMain/kotlin/ru/workinprogress/mani/today.kt
+++ shared/src/commonMain/kotlin/ru/workinprogress/mani/today.kt
@@ -1,7 +1,15 @@
+@file:OptIn(ExperimentalTime::class)
+
 package ru.workinprogress.mani
 
 import kotlinx.collections.immutable.*
-import kotlinx.datetime.*
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 fun <T> emptyImmutableList(): ImmutableList<T> = persistentListOf()
 fun <K, V> emptyImmutableMap(): ImmutableMap<K, V> = persistentMapOf()
